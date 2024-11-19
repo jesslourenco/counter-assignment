2812c88225a30cbd39db7ba7237b959725d17c40
jess <70455379+jesslourenco@users.noreply.github.com>
2024-11-19 13:39:38 -0500

    removed rendering component within beforeEach to follow React best practices

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,23 +1,21 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import Counter from "../components/Counter.js"
 
-beforeEach(() => {
-  // Render the Counter component here
-  render(<Counter />);
-})
-
 test('renders counter message', () => {
   // Looks for <h2>Counter</h2>
+  render(<Counter />);
   const counterLabel = screen.getByText("Counter", {exact: true});
   expect(counterLabel).toBeInTheDocument();
 });
 
 test('should render initial count with value of 0', () => {
+  render(<Counter />);
   const count = screen.getByTestId("count");
   expect(count).toHaveTextContent("0");
 });
 
 test('clicking + increments the count', () => {
+  render(<Counter />);
   const plusBtn = screen.getByText("+");
   fireEvent.click(plusBtn);
 
@@ -26,6 +24,7 @@ test('clicking + increments the count', () => {
 });
 
 test('clicking - decrements the count', () => {
+  render(<Counter />);
   const minusBtn = screen.getByText("-");
   fireEvent.click(minusBtn);
 
